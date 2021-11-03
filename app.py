9e65c68395c32864324395fdc450be1e76a7217c
Nicholas Livingstone <nlivingsto@unm.edu>
2021-11-03 14:04:24 -0600

    Improved modularity of zabbix api login

--- app.py
+++ app.py
@@ -8,47 +8,23 @@ from zabbix_util import *
 
 B146_GROUPID = os.getenv("B146_GROUPID")
 FE2065_GROUPID = os.getenv("FE2065_GROUPID")
-REFRESH_TIME = 5    # How often to refresh API data in minutes
+REFRESH_TIME = 5  # How often to refresh API data in minutes
 
 group_ids = {B146_GROUPID, FE2065_GROUPID}
-
 
 app = Flask(__name__)
 app.debug = True
 
-zapi = login_zabbix()
+zapi = ZabbixLogin()
 
 
 @app.route('/')
 def hello_world():
-    b146_data = get_api_data(B146_GROUPID)
-    fe2965_data = get_api_data(FE2065_GROUPID)
+    zapi.get_api_data(B146_GROUPID)
 
-    print(b146_data)
+    print(zapi.api_data)
 
-    return render_template('index.html', hostData=b146_data)
-
-
-def get_api_data(group_id):
-    hosts = list()
-
-    raw_host_data = zapi.host.get(output='extend', groupids=group_id)
-
-    for host in raw_host_data:
-        name = host['host']
-        cpu_util = zapi.item.get(hostids=host['hostid'], tags=[{"tag": "API"}])[0]['lastvalue']
-        cpu_util = round(float(cpu_util), 2)
-
-        host_dict = {
-            'name': name,
-            'cpu_util': cpu_util
-        }
-
-        hosts.append(host_dict)
-
-    hosts = sorted(hosts, key=lambda i: i['name'])
-
-    return hosts
+    return render_template('index.html', hostData=zapi.api_data)
 
 
 def thread_refresh_data():
--- zabbix_util.py
+++ zabbix_util.py
@@ -6,14 +6,33 @@ from threading import Thread
 from pyzabbix import ZabbixAPI
 
 
-def login_zabbix():
-    """
-    Login and Connect to Zabbix API
-    """
-    zapi = ZabbixAPI(os.getenv('ZABBIX_API'))
-    zapi.login(api_token=os.getenv('API_TOKEN'))
-    print("Connected to Zabbix API Version %s" % zapi.api_version())
-    return zapi
+class ZabbixLogin():
+    def __init__(self):
+        # Login and Connect to Zabbix API
+        self.login_instance = ZabbixAPI(os.getenv('ZABBIX_API'))
+        self.login_instance.login(api_token=os.getenv('API_TOKEN'))
+        print("Connected to Zabbix API Version %s" % self.login_instance.api_version())
 
+        # Initialize Data
+        self.api_data = None
+
+    def get_api_data(self, group_id):
+        hosts = list()
+
+        raw_host_data = self.login_instance.host.get(output='extend', groupids=group_id)
+
+        for host in raw_host_data:
+            name = host['host']
+            cpu_util = self.login_instance.item.get(hostids=host['hostid'], tags=[{"tag": "API"}])[0]['lastvalue']
+            cpu_util = round(float(cpu_util), 2)
+
+            host_dict = {
+                'name': name,
+                'cpu_util': cpu_util
+            }
+
+            hosts.append(host_dict)
+
+        self.api_data = sorted(hosts, key=lambda i: i['name'])
 
 
