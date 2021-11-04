97204e72bbaf531f910af7b1e60f169d6364029b
Nicholas Livingstone <nlivingsto@unm.edu>
2021-11-04 13:26:23 -0600

    Requests to update zabbix data are now made every 5 minutes. And added logging through flask.

--- app.py
+++ app.py
@@ -1,37 +1,28 @@
-import os
-import time
-from threading import Thread
-
-from flask import Flask, render_template
-
+from threading import Timer
+import logging
+from flask import Flask, render_template, logging as flog
 from zabbix_util import *
+
 
 REFRESH_TIME = 5  # How often to refresh API data in minutes
 
-
-
 app = Flask(__name__)
 app.debug = True
-
-zapi = ZabbixLogin()
-
+flog.default_handler.setFormatter(logging.Formatter("%(asctime)s: %(message)s"))
 
 @app.route('/')
 def hello_world():
-    zapi.get_api_data()
-
-    print(zapi.api_data[0])
-
     return render_template('index.html', hostData=zapi.api_data[0])
 
 
-def thread_refresh_data():
-    refresh = 60 * REFRESH_TIME
-    start_time = time.time()
-    while True:
-        logging.info("Updating API Data")
-        time.sleep(refresh - ((time.time() - start_time) % refresh))
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
 
+zapi = ZabbixLogin(app)
+zapi.get_api_data()
+RepeatTimer(60 * REFRESH_TIME, zapi.get_api_data).start()
 
 if __name__ == '__main__':
     app.run()
